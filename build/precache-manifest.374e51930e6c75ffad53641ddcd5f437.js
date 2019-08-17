26c1f76b17de56372c725e8ca3911b6736e16b72
malarc01 <45378877+malarc01@users.noreply.github.com>
2019-08-17 16:34:02 -0400

    deploy setup

---
+++ build/precache-manifest.374e51930e6c75ffad53641ddcd5f437.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d51881025d52ffeca342fdb7da29fd59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa8d4ce03da73b98b02e",
+    "url": "/static/css/2.11b452ed.chunk.css"
+  },
+  {
+    "revision": "f9fe4fda5a33036a894c",
+    "url": "/static/css/main.4d89ea0b.chunk.css"
+  },
+  {
+    "revision": "aa8d4ce03da73b98b02e",
+    "url": "/static/js/2.e830d547.chunk.js"
+  },
+  {
+    "revision": "f9fe4fda5a33036a894c",
+    "url": "/static/js/main.b82b6863.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
